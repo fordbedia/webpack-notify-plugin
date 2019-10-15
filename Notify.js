1ecb1ba1595a85e2ceb44f6cd3bded0fed4eb761
Ford Bedia <fordbedia@gmail.com>
2019-10-15 16:38:02 +0800

    Patched title attribute

--- Notify.js
+++ Notify.js
@@ -5,8 +5,9 @@ const pkg = require('./package.json')
 const notifier = require('node-notifier')
 
 class Notifier {
-  constructor(params = { message: '', icon: '' }) {
+  constructor(params = { title: '', message: '', icon: '' }) {
     if (Object.keys(params).length) {
+      this.title = params.title
       this.message = params.message
       this.icon = params.icon
     }
@@ -19,7 +20,7 @@ class Notifier {
       const time = ((stats.endTime - stats.startTime) / 1000).toFixed(2)
 
       notifier.notify({
-        title: pkg.name,
+        title: this.title || pkg.name,
         message: `${this.message || 'Woo Done!'}\n${
           stats.compilation.errors.length
         } errors in ${time}s`,
